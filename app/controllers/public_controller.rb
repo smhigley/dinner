556c8fa135ce620a6525bca41d9930413d1af051
smhigley <sarahmhigley@gmail.com>
2014-03-08 21:33:36 +0000

    Searches Flickr API for images matching :category

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,7 +1,22 @@
+require 'json'
+FlickRaw.api_key="5f6652a08b32dd50ae264fb3f47a9632"
+FlickRaw.shared_secret="6826da1b61cf3f39"
+
 class PublicController < ApplicationController
   def random
     num_items = Item.count
-    offset_items = Item.offset(rand(num_items))
-    @item = offset_items.first
+    offset = rand(num_items)    
+    @item = Item.first(:offset => offset)
+    
+    num_photos = 5
+    photos = flickr.photos.search :tags => @item.category, :per_page => num_photos
+    
+    #build url, format = http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+    index = rand(num_photos)
+    farm_id = photos[index]["farm"].to_s
+    server_id = photos[index]["server"]
+    id = photos[index]["id"]
+    secret = photos[index]["secret"]
+    @image_url = "http://farm" + farm_id + ".staticflickr.com/" + server_id + "/" + id + "_" + secret + ".jpg"
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# require the flickr gem
+require 'flickraw'
+FlickRaw.api_key="5f6652a08b32dd50ae264fb3f47a9632"
+FlickRaw.shared_secret="6826da1b61cf3f39"
+
 # Initialize the Rails application.
 Dinner::Application.initialize!
